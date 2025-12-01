d281b8ea86837298c9f71b3ea681b52ac13d689a
Gerardas Sabaliauskas <gerardas.sab@gmail.com>
2025-12-01 13:48:53 +0100

    Comienzo para el listado de categorías, y con documentación sobre los pasos que hay que realizar

--- app/composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/ui/categorias/CategoriasList.kt
+++ app/composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/ui/categorias/CategoriasList.kt
@@ -5,14 +5,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 
 
+//Hay que realizar el merge para poder realizar los casos de uso y más
 @Composable
 fun Categorias(
     categoriasViewModel: CategoriasViewModel,
     onClose: () -> Unit
 ) {
+    //val items by categoriasViewModel.items.collectAsState()
     Text("Categorias")
     Column(modifier = Modifier.fillMaxSize()) {
 
--- app/composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/ui/categorias/CategoriasViewModel.kt
+++ app/composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/ui/categorias/CategoriasViewModel.kt
@@ -1,7 +1,37 @@
 package ies.sequeros.com.dam.pmdm.administrador.ui.categorias
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ies.sequeros.com.dam.pmdm.administrador.aplicacion.dependientes.BorrarDependienteUseCase
+import ies.sequeros.com.dam.pmdm.administrador.aplicacion.dependientes.activar.ActivarDependienteUseCase
+import ies.sequeros.com.dam.pmdm.administrador.aplicacion.dependientes.actualizar.ActualizarDependienteUseCase
+import ies.sequeros.com.dam.pmdm.administrador.aplicacion.dependientes.cambiarpermisos.CambiarPermisosUseCase
+import ies.sequeros.com.dam.pmdm.administrador.aplicacion.dependientes.crear.CrearDependienteUseCase
+import ies.sequeros.com.dam.pmdm.administrador.aplicacion.dependientes.listar.ListarDependientesUseCase
+import ies.sequeros.com.dam.pmdm.commons.infraestructura.AlmacenDatos
+import kotlinx.coroutines.launch
 
-class CategoriasViewModel: ViewModel()
+class CategoriasViewModel(
+    //private val categoriaRepositorio: ICategoriaRepositorio,
+    val almacenDatos: AlmacenDatos
+) : ViewModel()
 {
+    //Implementar todos los usos de casos de uso de Categorias
+    //Conseguir los "items" dentro de categorias
+
+//    init {
+//        actualizarDependienteUseCase = ActualizarDependienteUseCase(dependienteRepositorio,almacenDatos)
+//        borrarDependienteUseCase = BorrarDependienteUseCase(dependienteRepositorio,almacenDatos)
+//        crearDependienteUseCase = CrearDependienteUseCase(dependienteRepositorio,almacenDatos)
+//        listarDependientesUseCase = ListarDependientesUseCase(dependienteRepositorio,almacenDatos)
+//        activarDependienteUseCase = ActivarDependienteUseCase(dependienteRepositorio,almacenDatos)
+//        cambiarPermisosUseCase= CambiarPermisosUseCase(dependienteRepositorio,almacenDatos)
+//        viewModelScope.launch {
+//            var items = listarDependientesUseCase.invoke()
+//            _items.value.clear()
+//            _items.value.addAll(items)
+//
+//        }
+//    }
+
 }
